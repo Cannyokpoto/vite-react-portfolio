d9b7a6b94699a5a314423d67459c4c46b4fbd098
unknown <promiseokpoto5050@gmail.com>
2024-09-27 16:43:27 +0100

    updated my skills

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -103,7 +103,7 @@ const AboutStyles = styled.div`
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        gap: 10px;
+        gap: 20px;
 
         p {
           font-size: 15px;
